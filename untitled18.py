a2b9a029835c1fef9623797c5f779426f08805a3
siddhart19 <77846186+siddhart19@users.noreply.github.com>
2021-12-24 21:54:42 +0530

    Add files via upload

---
+++ untitled18.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+root=Tk()
+root.title("Luck friend Wheel")
+root.geometry("500x500")
+
+label_id="id: 198271 290198"
+label_name="name: siddhart"
+label_birth="date of birth: 19 april 2011"
+label_pin_no="pin: 451478"
+label_adress="adress: disney land , hong kong"
+label_authriztation="authriztation to drive the following vehicles: 4"
+
+print(label_id)
+print(label_name)
+print(label_birth)
+print(label_pin_no)
+print(label_adress)
+print(label_authriztation)
+
+root.mainloop()
